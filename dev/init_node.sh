d860aa848188cae0a345aa3755d7b3508ea6e914
fkmatsuda <fabio@fkmatsuda.dev>
2024-07-02 13:17:06 -0300

    fix: fixes NODE_HOME

--- dev/init_node.sh
+++ dev/init_node.sh
@@ -7,4 +7,6 @@ nvm install $NODE_VERSION
 nvm alias default $NODE_VERSION
 nvm use default
 
+mkdir -p /usr/local/node
+
 ln -sfn $(dirname $(dirname "$(nvm which current)")) /usr/local/node/current
